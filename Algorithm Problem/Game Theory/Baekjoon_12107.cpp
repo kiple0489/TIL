603407c1aa46bcde75b301310897824be67ccb00
kiple0489 <kiple0489@gmail.com>
2024-10-12 22:35:26 +0900

    [FEAT] Add Game Theory problem

---
+++ Algorithm Problem/Game Theory/Baekjoon_12107.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    if (n == 1)
+    {
+        cout << "B";
+    }
+    else
+    {
+        cout << "A";
+    }
+}
